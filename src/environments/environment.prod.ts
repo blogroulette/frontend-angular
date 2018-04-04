abfe38a3c098300e9a5730750dd16ea23fd31f9f
lucaswiemers <lucas.wiemers@4x1.de>
2018-04-04 16:25:36 +0200

    Change production url to localhost:8080/blogroulette-jee

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ import env from './.env';
 export const environment = {
   production: true,
   version: env.npm_package_version,
-  serverUrl: 'https://api.blogroulette.net/',
+  serverUrl: 'http://localhost:8080/blogroulette-jee',
   defaultLanguage: 'en-US',
   supportedLanguages: [
     'en-US',
