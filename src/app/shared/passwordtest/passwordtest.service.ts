1536924196cf11f22972b82e1bcd8c7ab501a067
lucaswiemers <lucas.wiemers@4x1.de>
2018-04-13 17:45:11 +0200

    Caretaking in PasswordTestService.

--- src/app/shared/passwordtest/passwordtest.service.ts
+++ src/app/shared/passwordtest/passwordtest.service.ts
@@ -2,26 +2,24 @@ import { Component, OnInit, Injectable } from '@angular/core';
 import * as owaspPasswordStrengthTest from 'owasp-password-strength-test';
 
 @Injectable()
-export class PasswordTestService implements OnInit {
-checker = owaspPasswordStrengthTest;
+export class PasswordTestService {
+  checker = owaspPasswordStrengthTest;
 
   constructor() { }
 
-  ngOnInit() { }
-
-  TestPassword(pw: string): Check {
+  public TestPassword(pw: string): Check {
     return this.checker.test(pw);
   }
 
 }
 
 export interface Check {
-  errors:             string[];
-  failedTests:        number[];
+  errors: string[];
+  failedTests: number[];
   requiredTestErrors: string[];
   optionalTestErrors: string[];
-  passedTests:        number[];
-  isPassphrase:       boolean;
-  strong:             boolean;
+  passedTests: number[];
+  isPassphrase: boolean;
+  strong: boolean;
   optionalTestsPassed: number;
 }
